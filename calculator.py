5c4ee3fe3f719f58d56cce091b38515619e7d940
Kamall Dev <kamleshsingh12210@gmail.com>
2026-09-02 14:21:43 +0530

    Add calculator project

---
+++ calculator.py
@@ -0,0 +1,17 @@
+fnum = int(input("enter the 1 st number"))
+snum =  int(input("enter the 2 st number"))
+
+op = input("enter the opration")
+
+if op == "+":
+    print(fnum+snum)
+
+elif op == "-":
+    print(fnum-snum)
+
+elif op =="*":
+    print(snum*fnum)
+
+else:
+    print(fnum/snum)
+        
